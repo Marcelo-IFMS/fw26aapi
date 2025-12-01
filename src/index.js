5f06fed9e24ce07ddbe01cf66b88a6ab89bdb945
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-12-01 11:57:15 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require("body-parser");
 const app = express();
 const port = 3000
 
@@ -8,16 +6,11 @@ app.set("view engine", "ejs")
 app.set("views", "./views")
 
 
-app.use(bodyParser.json());
 /* estas linhas são para leitura de de body.json */
 app.use(express.json());
 app.use(express.urlencoded());
 /* aqui é o acesso exerno do sistema para a api */
-app.use(cors({
-  origin: ["https://portalnoticiasinfoa.vercel.app", "https://urban-winner-q7vq67647jrwh4g5w-3000.app.github.dev/"], // domínio do seu front
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization"
-}));
+
 
 //rotas
 require('../db/dbconnect')(app);
